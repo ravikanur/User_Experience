9f65c344975035dcbe80263c8923ed82b0b133bf
ravikanur <ravikanur@gmail.com>
2023-10-09 23:36:41 +0530

    updated prediction pipeline

--- src/constants/prediction_pipeline/__init__.py
+++ src/constants/prediction_pipeline/__init__.py
@@ -2,3 +2,6 @@ INPUT_DIR: str = 'input'
 
 PREDICTION_DIR: str = 'prediction'
 
+REQUIRED_COLUMNS: list = ['specifictime','indicator1','indicator2','indicator3','indicator4',
+                            'indicator5','indicator6','indicator7','indicator8']
+
--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,13 +1,14 @@
 import os, sys
 
 from src.config.spark_manager import spark_session
+from src.constants.prediction_pipeline import *
 from src.components.data_validation import DataValidation
 
 from src.logger import logging
 from src.exception import UserException
 
 class PredictionPipeline:
-    def read_data(self):
+    def read_data(self, path:str):
         try:
             logging.info("Entered read_data method")
         except Exception as e:
@@ -16,6 +17,26 @@ class PredictionPipeline:
     def get_valid_invalid_files(self):
         try:
             logging.info("Entered get_valid_invalid_files method")
+            user_list = os.listdir(INPUT_DIR)
+
+            valid_files, invalid_files = [], []
+
+            for user in user_list:
+                user_path = os.path.join(INPUT_DIR, user)
+
+                temp_df = self.read_data(user_path)
+
+                cols = temp_df.column
+
+                no_col_list = [col for col in REQUIRED_COLUMNS if col not in cols]
+
+                if len(no_col_list) > 0:
+                    invalid_files.append(user)
+                else:
+                    valid_files.append(user)
+            
+                
+
         except Exception as e:
             logging.error(e)
             raise UserException(e, sys)
