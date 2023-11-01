6175cb1a255c2aad6fbf9b38014c2c65c4ce05e0
ravikanur <ravikanur@gmail.com>
2023-11-01 14:24:41 +0530

    Updated Prediction Pipeline

--- src/constants/prediction_pipeline/__init__.py
+++ src/constants/prediction_pipeline/__init__.py
@@ -5,3 +5,5 @@ PREDICTION_DIR: str = 'prediction'
 REQUIRED_COLUMNS: list = ['specifictime','indicator1','indicator2','indicator3','indicator4',
                             'indicator5','indicator6','indicator7','indicator8']
 
+
+PRED_MODEL_PATH: str = "./model/best_model/UE_model"
--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,11 +1,12 @@
 import os, sys
 
 from pyspark.sql import dataframe
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import lit, col
+from pyspark.ml.pipeline import PipelineModel
 from src.config.spark_manager import spark_session
 from src.constants.prediction_pipeline import *
 from src.constants.training_pipeline import *
-from src.components.data_validation import DataValidation
+from src.components.data_validation import add_mean_indicator_col_per_user
 
 from src.logger import logging
 from src.exception import UserException
@@ -29,7 +30,7 @@ class PredictionPipeline:
 
                 user_name = user.split(sep='.')[0]
 
-                temp_df: dataframe = spark_session.read.csv(path, inferSchema=True, header=True)
+                temp_df: dataframe = spark_session.read.csv(user_path, inferSchema=True, header=True)
 
                 cols = temp_df.columns
 
@@ -52,7 +53,28 @@ class PredictionPipeline:
     def initiate_batch_prediction(self):
         try:
             logging.info("Entered initiate_batch_prediction method")
-            
+            valid_files, invalid_files, final_df = self.get_valid_invalid_files()
+
+            for column in INDICATOR_COLS:
+                final_df = final_df.filter(col(column) < INDICATOR_THRESHOLD)
+
+            final_df = final_df.withColumn(DATE_VAL_STRING, col(DATE_VAL_STRING).cast('string'))
+
+            final_df = add_mean_indicator_col_per_user(final_df, USER_COLUMN_NAME, INDICATOR_COLS)
+
+            final_df = final_df.drop(*COLS_TO_BE_REMOVED)
+
+            final_df = final_df.withColumn(TARGET_COLUMN_NAME, lit('UBE'))
+
+            model = PipelineModel.load(PRED_MODEL_PATH)
+
+            pred = model.transform(final_df)
+
+            pred.write.mode('append').parquet('./output/pred.parquet')
         except Exception as e:
             logging.error(e)
             raise UserException(e, sys)
+
+if __name__ == '__main__':
+    pred_pipeline = PredictionPipeline()
+    pred_pipeline.initiate_batch_prediction()
