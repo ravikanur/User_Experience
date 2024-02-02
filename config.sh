825d3ab8af3817f9cad4b8e68f1cd7368d3bea71
ravikanur <ravikanur@gmail.com>
2024-02-02 11:42:21 +0530

    updated appspec file

---
+++ config.sh
@@ -0,0 +1,4 @@
+export AIRFLOW_HOME="/config/workspace/airflow"
+export AIRFLOW__CORE__DAGBAG_IMPORT_TIMEOUT=1000
+export AIRFLOW__CORE__ENABLE_XCOM_PICKLING=True
+export PATH="$PATH:/config/.local/bin"
